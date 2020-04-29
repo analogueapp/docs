151bd261afb9ac165c1dea4159ef064080b1cbf2
J03L R0J0 <rojo@analogue.app>
2020-04-28 22:11:02 -0700

    ⚡️ Update sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,12 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+  api: {
+    About: ['api'],
+    Authentication: [
+      'auth/token',
+      'auth/authorization'
+    ],
+    Log: [
+      'log/parse',
+    ]
   },
 };
